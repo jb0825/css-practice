a261b7b6c6a72758c2072de1fc27e5fca4843b47
jb <jebi0825@naver.com>
2022-02-11 16:59:41 +0900

    commit

--- 01/index.js
+++ 01/index.js
@@ -1,8 +1,23 @@
-document.querySelector(".circle-button").onclick = function () {
-  let target = document.querySelector(".back-1");
-  let state = target.getAttribute("state");
+const playIcon = document.querySelector(".icon-play");
+const pauseIcon = document.querySelector(".icon-pause");
 
-  if (target.classList.toggle(state)) {
-    document.querySelector(".icon-play").style.display = "inline-block";
-  }
+/* play-pause button */
+function toggleIcon(active, inactive) {
+  active.style.display = "inline-block";
+  inactive.style.display = "none";
+}
+
+document.querySelector(".circle-button").onclick = function () {
+  if (document.querySelector(".back-1").classList.toggle("pause")) toggleIcon(playIcon, pauseIcon);
+  else toggleIcon(pauseIcon, playIcon);
+};
+
+const rangeBar = document.querySelector(".range-bar");
+const rangeBtn = document.querySelector(".range-btn");
+const rangeTooltip = document.querySelector(".range-tooltip");
+
+rangeBtn.onmousedown = function (e) {};
+
+window.onmousemove = function (e) {
+  e.preventDefault();
 };
